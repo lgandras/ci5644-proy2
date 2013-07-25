8aefd4cf3e70765a048a1e2ae95196b8686d225a
Andras Gyomrey <lgandras@gmail.com>
2013-07-24 20:22:12 -0430

    agregar twitter bootstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new KDA\BlogBundle\KDABlogBundle(),
+            new Bc\Bundle\BootstrapBundle\BcBootstrapBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
